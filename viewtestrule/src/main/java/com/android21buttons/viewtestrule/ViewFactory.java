10ce79b079b76ed4b0bcbd63231d74fc012e429b
Brais Gabín <braisgabin@gmail.com>
2017-11-23 11:14:55 +0100

    Give Activity instead of Context in ViewFactory.

--- viewtestrule/src/main/java/com/android21buttons/viewtestrule/ViewFactory.java
+++ viewtestrule/src/main/java/com/android21buttons/viewtestrule/ViewFactory.java
@@ -1,10 +1,11 @@
 package com.android21buttons.viewtestrule;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 public interface ViewFactory<T extends View> {
 
-    T create(Context context, ViewGroup parent);
+    T create(Activity activity, ViewGroup parent);
 }
